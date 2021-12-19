ca30e1cf917b982caec4642f8b96c58038809cb8
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2021-12-19 22:17:06 +0100

    Code update

--- cardGame/index.js
+++ cardGame/index.js
@@ -1,11 +1,13 @@
-let result = document.getElementById("result")
-
 let firstCard = 10
 let secondCard = 8
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false // check if a gamer wins
 let isAlive = true // Check whether a gamer is still in the game.
 let message = "" // store the result of gamers game.
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 // The logic of the game
 
@@ -14,8 +16,12 @@ says execute A if condition A is true. otherwise execute B
 */
 
 // A start game function that allows users to start the game
-
 function startGame() {
+    renderGame()
+}
+
+
+function renderGame() {
     if (sum < 21) {
         message = "Do you want to draw a new Card?"
     } else if (sum === 21) {  //using triple equal signs is more stricter than double
@@ -28,5 +34,15 @@ function startGame() {
         isAlive = false
     }
     
-    result.textContent = "Result: " + message
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+}
+
+// create a new card function
+function newCard() {
+    let card = 6
+    sum += card
+    cards.push(card)
+    startGame()
 }
