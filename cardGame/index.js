b7add97867ea9d26bd807b779ef2d1b65aaaf417
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2021-12-25 22:46:46 +0100

    Final code for game

--- cardGame/index.js
+++ cardGame/index.js
@@ -1,13 +1,35 @@
-let firstCard = 10
-let secondCard = 8
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+// declaring variables
+let player = {
+    name: "Lawal",
+    chips: 1000
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false // check if a gamer wins
-let isAlive = true // Check whether a gamer is still in the game.
+let isAlive = false // Check whether a gamer is still in the game.
 let message = "" // store the result of gamers game.
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+
+
+//Display the player object.
+playerEl.textContent = player.name + ": $" + player.chips
+
+
+// create a function to get random number
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 // The logic of the game
 
@@ -17,10 +39,15 @@ says execute A if condition A is true. otherwise execute B
 
 // A start game function that allows users to start the game
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
-
+// Create a function that tell us the outcome of a round
 function renderGame() {
     if (sum < 21) {
         message = "Do you want to draw a new Card?"
@@ -36,13 +63,19 @@ function renderGame() {
     
     messageEl.textContent = message
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
 }
 
 // create a new card function
 function newCard() {
-    let card = 6
-    sum += card
-    cards.push(card)
-    startGame()
+    // condition that makes sure only player who is still in the game can ask for a new card
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
