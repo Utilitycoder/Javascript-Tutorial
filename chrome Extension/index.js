6d5b86416f9410e1a845555104ce0a0cfc028d21
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2021-12-30 13:01:06 +0100

    Update

--- chrome Extension/index.js
+++ chrome Extension/index.js
@@ -1,9 +1,17 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // alternative to innerHTML
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+}
