5fe9a409669189608c9d72c92765bad8750962ef
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2021-12-31 09:31:35 +0100

    Updated

--- chrome Extension/index.js
+++ chrome Extension/index.js
@@ -1,21 +1,24 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-// reduce DOM operation
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
-    // alternative to innerHTML
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
-}
+function renderLeads() {
+    // reduce DOM operation
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li><a target='blank' href='" + myLeads[i] +"'>" + myLeads[i] + "</a></li>"
+        // alternative to innerHTML
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
 
-ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems
+}
