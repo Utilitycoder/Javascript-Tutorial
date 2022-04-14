7ad50d22ec92437981f14a9558038c91a9d904cd
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-04-14 16:36:38 +0100

    update

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,6 +1,6 @@
 // Created DOM variables
 let inputEl=document.getElementById("input-el")
-let pass1El=document.getElementById("pass1-el");
+let pass1El=document.getElementById("pass");
 let pass2El=document.getElementById("pass2-el");
 let pass3El=document.getElementById("pass3-el");
 let pass4El=document.getElementById("pass4-el");
